b421c3cf3c571fd1671b0b131e956158645b7121
mosh <mosh.ohayo@gmail.com>
2024-12-21 18:25:13 +0900

    pnpm format

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    rules: {
-      ...pluginReactHooks.configs.recommended.rules,
-    }
-  },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"]
+	{
+		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+		plugins: {
+			"react-hooks": pluginReactHooks,
+		},
+		rules: {
+			...pluginReactHooks.configs.recommended.rules,
+		},
+	},
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	pluginReact.configs.flat["jsx-runtime"],
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import checker from 'vite-plugin-checker'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({}),
-    react(),
-    checker({
-      typescript: true
-    }),
-  ],
-})
+	plugins: [
+		TanStackRouterVite({}),
+		react(),
+		checker({
+			typescript: true,
+		}),
+	],
+});
