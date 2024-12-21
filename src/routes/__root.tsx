34d6647f25b4b3e2d15e89d358452ecf21515056
mosh <mosh.ohayo@gmail.com>
2024-12-21 20:51:15 +0900

    Add tailwindcss

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import "./__root.css";
 
 export const Route = createRootRoute({
 	component: RootComponent,
