467df68914f2b98c9cc9cdf4bf00e880ddef6c50
mosh <mosh.ohayo@gmail.com>
2024-12-30 19:39:28 +0900

    Add unicorn to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -16,13 +17,19 @@ export default [
 		},
 		settings: {
 			react: {
-				version: "detect"
-			}
-		}
+				version: "detect",
+			},
+		},
 	},
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	eslintPluginUnicorn.configs["flat/recommended"],
+	{
+		rules: {
+			"unicorn/no-abusive-eslint-disable": "off",
+		},
+	},
 	pluginReact.configs.flat.recommended,
 	pluginReact.configs.flat["jsx-runtime"],
 ];
